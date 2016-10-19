70727aa3daae5084c6465bbd9af32be5f89caf1d
John Doe <nobody@nowhere.net>
2016-10-19 00:26:09 -0400

    Bug fixes and improvements to the demo program.

--- exif.lisp
+++ exif.lisp
@@ -3,6 +3,8 @@
 
 (in-package :exif)
 
+(declaim (optimize (debug 3) (speed 0)))
+
 (define-enum tiff-type 2 (:byte-order :dynamic)
   (:unsigned-byte 1)
   :ascii
@@ -23,6 +25,11 @@
 
 (defvar *pending-buffers* nil)
 (defvar *tiff-start* nil)
+
+(defun slot-path (object &rest slots)
+  (loop for slot in slots do
+       (setf object (slot-value object slot)))
+  object)
 
 (defun no-writer (obj stream)
   (declare (ignore obj stream))
@@ -49,6 +56,9 @@
      (write-integer (denominator value) 4 stream :byte-order *byte-order*
 		    :signed signed))))  
 
+(defun ensure-non-null-pointer (pointer-value)
+  (assert (/= 0 pointer-value)))
+
 (defun tiff-type->defbinary-type (type)
   (ecase type
     ((:unsigned-long :undefined) '(unsigned-byte 32))
@@ -67,17 +77,36 @@
     (:unsigned-short '(unsigned-byte 16))))
 
 (defbinary directory-entry (:byte-order :dynamic)
+  (file-position 0 :type file-position)
   (tag 0 :type (unsigned-byte 16))
   (type 0 :type tiff-type)
   (count 0 :type (unsigned-byte 32) :writer (lambda (obj stream)
 					      (declare (ignore obj))
-					      (setf count (length value))
+					      (setf count (if (eq type :ascii)
+							      (1+ (length value))
+							      (if (or (listp value)
+								      (vectorp value))
+								  (length value)
+								  1)))
 					      (write-integer count 4 stream :byte-order *byte-order*)))
   (value 0 :type (eval
-		  (cond ((> count 1)
+		  ;; TIFF requires that ANY value that can fit within 4 bytes
+		  ;; be represented as an immediate value. That includes array
+		  ;; types.
+		  (cond ((and (eq type :ascii)
+			      (<= count 4))
+			 (tiff-type->defbinary-type :ascii))
+			((or (and (member type '(:signed-byte :unsigned-byte))
+				  (<= count 4))
+			     (and (member type '(:signed-short :unsigned-short))
+				  (<= count 2)))
+			 `(simple-array ,(tiff-type->defbinary-type type)
+					(,count)))
+			((> count 1)
 			 `(pointer :pointer-type (unsigned-byte 32)
 				   :data-type ,(tiff-type->defbinary-type type)
 				   :base-pointer-name 'tiff-base-pointer
+				   :validator #'ensure-non-null-pointer
 				   :region-tag 'tiff-region))
 			((member tag '(34665 ;; EXIF
 				       34853 ;; GPS
@@ -85,21 +114,27 @@
 			 `(pointer :pointer-type (unsigned-byte 32)
 				   :data-type tiff-image-file-directory
 				   :base-pointer-name 'tiff-base-pointer
+				   :validator #'ensure-non-null-pointer
 				   :region-tag 'tiff-region))
 			(t
 			 (case type
 			   ((:undefined)
 			    '(unsigned-byte 32))
-			   ((:double-float :ascii :signed-rational :unsigned-rational)
+			   ((:double-float :signed-rational :unsigned-rational :ascii)
 			    `(pointer :pointer-type (unsigned-byte 32)
 				      :data-type ,(tiff-type->defbinary-type type)
 				      :base-pointer-name 'tiff-base-pointer
+				      :validator #'ensure-non-null-pointer
 				      :region-tag 'tiff-region))
 			   (otherwise (tiff-type->defbinary-type type)))))))
   (padding 0 :type (eval (if (> count 1)
 			     'null
 			     (ecase type
-			       ((:unsigned-long :unsigned-rational :signed-rational :double-float :ascii :signed-long
+			       (:ascii
+				(if (>= count 4)
+				    'null
+				    `(unsigned-byte ,(* 8 (- 4 count)))))
+			       ((:unsigned-long :unsigned-rational :signed-rational :double-float :signed-long
 						:single-float :undefined)
 				'null)
 			       ((:signed-byte :unsigned-byte)
@@ -112,6 +147,9 @@
   (directory-entries #() :type (counted-array 2 directory-entry))
   (next-directory-offset 0 :type (unsigned-byte 32)))
 
+(defun write-integer/debug (&rest arguments)
+  (format t "Calling ~S~%" (cons 'write-integer arguments))
+  (apply #'write-integer arguments))
 
 (defbinary tiff (:byte-order :dynamic :preserve-*byte-order* nil)
   (tiff-base-pointer 0 :type base-pointer)
@@ -121,48 +159,46 @@
 						       2)))
   (magic 42 :type (magic :actual-type (unsigned-byte 16)
 			 :value 42))
+  (offset-ptr 0 :type file-position)
   (first-image-file-directory-offset 0 :type (unsigned-byte 32))
-  (offset-ptr 0 :type (unsigned-byte 32)
-	      :reader (lambda (stream)
-			(declare (optimize (debug 3) (speed 0)))
-			(values
-			 (- (file-position stream) 4)
-			 0))
-	      :writer (lambda (obj stream)
-			(declare (ignore obj))
-			(setf offset-ptr (- (file-position stream) 4))
-			0))
   (image-directories nil :type list
 		     :reader (lambda (stream)
 			       (let* ((next-directory nil)
 				      (byte-count 0)
 				      (directories
-				       (loop for offset = first-image-file-directory-offset
-					  then (slot-value next-directory 'next-directory-offset)
-					  until (= offset 0)
-					  collect (progn
-						    (file-position stream (+ offset tiff-base-pointer))
-						    (setf next-directory
-							  (multiple-value-bind (dir bytes)
-							      (read-binary 'tiff-image-file-directory stream)
-							    (incf byte-count bytes)
-							    dir))))))
+				       (with-file-position (0 stream)
+					 (loop for offset = first-image-file-directory-offset
+					    then (slot-value next-directory 'next-directory-offset)
+					    until (= offset 0)
+					    collect (progn
+						      (file-position stream (+ offset tiff-base-pointer))
+						      (setf next-directory
+							    (multiple-value-bind (dir bytes)
+								(read-binary 'tiff-image-file-directory stream)
+							      (incf byte-count bytes)
+							      dir)))))))
 				 (values directories byte-count)))
 		     :writer (lambda (obj stream)
 			       (declare (ignore obj))
 			       (force-output stream)
 			       (let ((real-offset (file-length stream)))
-				 (file-position stream offset-ptr)
-				 (write-integer (- real-offset tiff-base-pointer) 4 stream :byte-order *byte-order*)
-				 (setf first-image-file-directory-offset real-offset)
-				 (file-position stream real-offset)
-				 (loop for (dir . more-dirs) on image-directories sum
-				      (let ((bytes (write-binary dir stream))
-					    (new-eof (file-position stream)))
-					(force-output stream)
-					(file-position stream (- new-eof 4))
-					(write-integer (if more-dirs new-eof 0) 4 stream :byte-order *byte-order*)
-					bytes)))))
+				 (with-file-position (offset-ptr stream)
+				   (write-integer (- real-offset tiff-base-pointer) 4 stream :byte-order *byte-order*)
+				   (setf first-image-file-directory-offset (- real-offset tiff-base-pointer))
+				   (file-position stream real-offset)
+				   ;; FIXME: This is writing something incorrectly. I'm getting errors when
+				   ;;        trying to read it back in. I've determined that everything
+				   ;;        is being written correctly up until this point. Then it all
+				   ;;        goes pear-shaped.
+				   (loop for (dir . more-dirs) on image-directories sum
+					(let ((bytes (write-binary dir stream))
+					      (new-eof (file-position stream)))
+					  (force-output stream)
+					  (file-position stream (- new-eof 4))
+					  (write-integer (if more-dirs
+							     (- new-eof tiff-base-pointer)
+							     0) 4 stream :byte-order *byte-order*)
+					  bytes))))))
   (tiff-region 0 :type (region-tag :base-pointer-name 'tiff-base-pointer)))
 				    
 
@@ -209,12 +245,8 @@
 			     (t `(simple-array (unsigned-byte 8) (,(- (slot-value tag 'length) 2)))))))
   (file-positioner nil :type (custom :reader
 				     (lambda (stream)
-				       (values
-					(and (not (jpeg-tag-no-length-p tag))
-					     (file-position stream
-							    (+ (slot-value tag 'length-offset)
-							       (slot-value tag 'length))))
-					0))
+				       (declare (ignore stream))
+				       (values nil 0))
 				     :writer
 				     (lambda (obj stream)
 				       (declare (ignore obj))
@@ -246,3 +278,17 @@
 	(loop for segment in jpeg-segments
 	   do (write-binary segment out))
 	(write-bytes rest-of-image out)))))
+
+(defun read/write-test (pathname object type &key (base-pointer 0) tags)
+  (with-local-pointer-resolving-context
+    (with-open-binary-file (out pathname
+				:direction :io
+				:if-exists :supersede
+				:if-does-not-exist :create)
+      (write-binary-type object
+			 type out)
+      (loop for tag in tags
+	 do (lisp-binary::dump-tag tag base-pointer out)))
+    (with-open-binary-file (in pathname
+			       :direction :input)
+      (read-binary-type type in))))
