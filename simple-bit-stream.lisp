8a8b81a2a224eebba4486c0f84e96c6d1c2fe332
Jeremy Phelps <jeremy@stockwell.ai>
2023-07-14 02:14:24 -0400

    Fix a bug and write a test for the :include option.

--- simple-bit-stream.lisp
+++ simple-bit-stream.lisp
@@ -57,7 +57,10 @@ can be discarded if BYTE-ALIGNED-P returns T."))
     (cond
       ((= (slot-value stream 'bits-left) 0)
        (setf (slot-value stream 'last-byte)
-	     (read-byte (slot-value stream 'real-stream)))
+	     (restart-case (read-byte (slot-value stream 'real-stream))
+	       (continue ()
+		 :report "Pretend the read returned a 0 byte."
+		 0)))
        (setf (slot-value stream 'bits-left)
 	     (slot-value stream 'element-bits))
        (read-partial-byte/big-endian bits stream))
@@ -80,7 +83,10 @@ can be discarded if BYTE-ALIGNED-P returns T."))
     (cond
       ((= (slot-value stream 'bits-left) 0)
        (setf (slot-value stream 'last-byte)
-	     (read-byte (slot-value stream 'real-stream)))
+	     (restart-case (read-byte (slot-value stream 'real-stream))
+	       (continue ()
+		 :report "Pretend the read returned a 0 byte."
+		 0)))
        (setf (slot-value stream 'bits-left)
 	     (slot-value stream 'element-bits))
        (read-partial-byte/little-endian bits stream))
